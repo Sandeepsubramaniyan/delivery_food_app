b8bc817d42bf0d45b38d15342572d02b01ea6fa3
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-04-07 22:40:20 +0530

    created views using ModelViewSet

--- backend/zoggy/views.py
+++ backend/zoggy/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ZoggySerializer
+from .models import Zoggy
 
 # Create your views here.
+
+class ZoggyViewSet(ModelViewSet):
+    
+    serializer_class = ZoggySerializer
+    queryset = Zoggy.objects.all()
+    
