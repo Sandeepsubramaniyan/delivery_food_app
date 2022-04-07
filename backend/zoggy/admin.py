999b43e2d9eb063675ca3cc32045a340d9968b00
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-04-07 23:36:57 +0530

    added get func in view

--- backend/zoggy/admin.py
+++ backend/zoggy/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Zoggy
+
+admin.site.register(Zoggy)
 
 # Register your models here.
--- backend/zoggy/views.py
+++ backend/zoggy/views.py
@@ -2,11 +2,15 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import ZoggySerializer
 from .models import Zoggy
+from rest_framework.response import Response
 
 # Create your views here.
 
 class ZoggyViewSet(ModelViewSet):
     
-    serializer_class = ZoggySerializer
-    queryset = Zoggy.objects.all()
+    def get(self,request):
+        serializer_class = ZoggySerializer
+        queryset = Zoggy.objects.all()
+    
+        return Response (serializer_class.data)
     
