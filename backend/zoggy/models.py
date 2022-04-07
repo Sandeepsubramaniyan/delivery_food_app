29af255cb467399584b7c9c8f3b1f817044dbb36
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-04-07 21:56:02 +0530

    created models

--- backend/zoggy/models.py
+++ backend/zoggy/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Zoggy(models.Model):
+    id= models.IntegerField
+    hotel= models.CharField(max_length=50)
+    cuisine= models.CharField(max_length=100)
+    location=models.CharField(max_length=100)
