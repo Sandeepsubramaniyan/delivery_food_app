edee07c6bdb4e414984505e7650f550904158131
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-04-14 09:47:32 +0530

    integrated backend and frontend

--- backend/zoggy/views.py
+++ backend/zoggy/views.py
@@ -8,13 +8,11 @@ from rest_framework.response import Response
 # Create your views here.
 
 class ZoggyViewSet(APIView):
-                
-        def get(self,request):
+    def get(self,request):                
+        display = Zoggy.objects.all()
+        serializer_class = ZoggySerializer(display,many=True)
+        return Response(serializer_class.data)
 
-                
-            display = Zoggy.objects.all()
-            serializer_class = ZoggySerializer(display,many=True)
-            return Response(serializer_class.data)
         
     
         
